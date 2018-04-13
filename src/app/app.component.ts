a3b39c5535a03d265e2bb24cd320b8991bd6deef
ahmetacer <dev>
2018-04-13 11:37:57 +0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  tcknControl = new FormControl('', [validateKN]);
+}
+function validateKN (control: FormControl) {
+  if (control.value) {
+    if (control.value.toString().length === 11) {
+      const value = control.value.toString();
+      const isEleven = /^[0-9]{11}$/.test(value);
+      let totalX = 0;
+      for (let i = 0 ; i < 10 ; i++) {
+        totalX += Number(value.substr(i, 1));
+      }
+      const isRuleX = totalX % 10 === Number(value.substr(10, 1));
+      let totalY1 = 0;
+      let totalY2 = 0;
+      for (let i = 0 ; i < 10 ; i += 2) {
+        totalY1 += Number(value.substr(i, 1));
+      }
+      for (let i = 1 ; i < 10 ; i += 2) {
+        totalY2 += Number(value.substr(i, 1));
+      }
+      const isRuleY = ((totalY1 * 7) - totalY2) % 10 === Number(value.substr(9, 0));
+      if (isEleven && isRuleX && isRuleY) {
+        return null;
+      } else {
+        return {'valid': false};
+      }
+    } else {
+      return {'valid': false};
+    }
+  } else {
+    return {'valid': false};
+  }
 }
